898e85aed0e372cc07dff4d5725d94f0ab53875a
ths <thalitasanttos77@gmail.com>
2025-09-02 10:28:33 -0300

    Initialize Gradle wrapper and project structure for MultiActivity

---
+++ MultiActivity/app/src/main/java/com/example/multiactivity/DetailActivity.kt
@@ -0,0 +1,25 @@
+package com.example.multiactivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val closeButton = findViewById<Button>(R.id.closeButton)
+        closeButton.setOnClickListener(){
+            finish()
+        }
+    }
+}
